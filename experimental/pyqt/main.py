e82f888392d9222d0a88bee2a67fed0dfc1b6e95
Caleb Hamilton <cjlhamilton@gmail.com>
2018-10-04 23:28:51 +0100

    Added code to load conversations from Facebook archive and refactored program

--- experimental/pyqt/main.py
+++ experimental/pyqt/main.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import json
 
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QWidget, \
     QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QListWidgetItem
@@ -7,59 +9,135 @@ from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont
 from ui_mainwindow import Ui_MainWindow
 
 
+class Conversation(object):
+    def __init__(self, participants, path, media_path, title,
+                 is_still_participant, thread_type):
+        self.participants = participants
+        self.path = path
+        self.media_path = media_path
+        self.title = title
+        self.is_still_participant = is_still_participant
+        self.thread_type = thread_type
+
+
+class ConversationsQMainWindow(QMainWindow):
+    def __init__(self, conversations):
+        super(ConversationsQMainWindow, self).__init__()
+        ui = Ui_MainWindow()
+        ui.setupUi(self)
+
+        conversationsStyleSheet = """
+            QListWidget {
+                outline: none;
+            }
+
+            QListWidget::item:selected,
+            QListWidget::item:focus {
+                background-color: #EEEEEE;
+                border: none;
+            }
+        """
+        ui.conversationsList.setStyleSheet(conversationsStyleSheet)
+
+        populate_conversations_list(ui, conversations)
+
+
 class ConversationsListQWidget(QWidget):
-    def __init__(self, name, bottom_text, parent=None):
+    def __init__(self, title, recent_message, parent=None):
         super(ConversationsListQWidget, self).__init__(parent)
 
         self.row = QVBoxLayout()
 
-        name_font = QFont("Sans Serif", 10)
-        name_label = QLabel(name)
-        name_label.setFont(name_font)
-        self.row.addWidget(name_label)
+        title_length = 24
+        if (len(title) > title_length):
+            title = title[0:title_length - 3] + "..."
 
-        bottom_text_font = QFont("Sans Serif", 9)
-        bottom_text_label = QLabel(bottom_text)
-        bottom_text_label.setFont(bottom_text_font)
-        bottom_text_label.setStyleSheet("QLabel {color: #555555;}")
-        self.row.addWidget(bottom_text_label)
+        title_font = QFont("Sans Serif", 10)
+        title_label = QLabel(title)
+        title_label.setFont(title_font)
+        self.row.addWidget(title_label)
+
+        preview_length = 28
+        if (len(recent_message) > preview_length):
+            recent_message = recent_message[0:preview_length - 3] + "..."
+
+        preview_font = QFont("Sans Serif", 9)
+        preview_label = QLabel(recent_message)
+        preview_label.setFont(preview_font)
+        preview_label.setStyleSheet("QLabel {color: #555555;}")
+        self.row.addWidget(preview_label)
 
         self.setLayout(self.row)
 
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(window)
+def get_fb_dir():
+    data_dir = "../../data"
+    fb_dir = None
+    for dirname in next(os.walk(data_dir))[1]:
+        if (dirname[0:9] == "facebook-"):
+            fb_dir = data_dir + "/" + dirname
+    return fb_dir
 
-conversationsStyleSheet = """
-    QListWidget {
-        outline: none;
-    }
 
-    QListWidget::item:selected,
-    QListWidget::item:focus {
-        background-color: #EEEEEE;
-        border: none;
-    }
-"""
-ui.conversationsList.setStyleSheet(conversationsStyleSheet)
+def get_conversations_list(fb_dir):
+    messages_dir = fb_dir + "/messages"
+    special_dirs = ["stickers_used"]
+    conversations = []
+    for dirname in next(os.walk(messages_dir))[1]:
+        # For each conversation, there is an all-lowercase directory containing
+        # the messages.json file. When a conversation contains media there is
+        # a separate directory with the same name, except that the directory's
+        # name is in PascalCase. So we check that the current directory is
+        # all-lowercase.
+        if (dirname.lower() != dirname or dirname in special_dirs):
+            continue
 
-entries = [
-    ("Person One", "Some first text..."),
-    ("Person Two", "Some second text..."),
-    ("Person Three", "Some third text..."),
-    ("Person Four", "Some fourth text...")
-]
+        path = messages_dir + "/" + dirname
+        with open(path + "/message.json") as f:
+            messages_data = json.load(f)
 
-for e in entries:
-    widget = ConversationsListQWidget(e[0], e[1])
+        participants = []
+        for participant in messages_data["participants"]:
+            participants.append(participant["name"])
 
-    item = QListWidgetItem(ui.conversationsList)
-    item.setSizeHint(widget.sizeHint())
+        media_path = messages_dir + "/" + messages_data["thread_path"]
 
-    ui.conversationsList.addItem(item)
-    ui.conversationsList.setItemWidget(item, widget)
+        # No title if conversation is with a deleted user
+        title = messages_data.get("title", "Facebook User")
 
-window.show()
-sys.exit(app.exec_())
+        conversations.append(
+            Conversation(participants, path, media_path, title,
+                         messages_data["is_still_participant"],
+                         messages_data["thread_type"]))
+    return conversations
+
+
+def populate_conversations_list(ui, conversations):
+    for conversation in conversations:
+        widget = ConversationsListQWidget(conversation.title,
+                                          conversation.media_path)
+
+        item = QListWidgetItem(ui.conversationsList)
+        item.setSizeHint(widget.sizeHint())
+
+        ui.conversationsList.addItem(item)
+        ui.conversationsList.setItemWidget(item, widget)
+
+
+if (__name__ == "__main__"):
+    app = QApplication(sys.argv)
+
+    fb_dir = get_fb_dir()
+    if (fb_dir is None):
+        print("Facebook archive directory not found.")
+        exit()
+
+    print("Facebook directory:", fb_dir)
+
+    conversations = get_conversations_list(fb_dir)
+    print("Loaded conversations list.")
+
+    window = ConversationsQMainWindow(conversations)
+    window.show()
+
+    sys.exit(app.exec_())
